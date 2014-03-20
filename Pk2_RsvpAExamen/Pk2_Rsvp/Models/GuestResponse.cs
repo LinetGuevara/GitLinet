5c843a6d1a8d1baf4d9c3a4ac3e2c895bf321731
Linet <lilian_01_6@hotmail.com>
2014-03-20 17:58:23 -0600

    Me ha Visto

---
+++ Pk2_RsvpAExamen/Pk2_Rsvp/Models/GuestResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//usando para validar campo
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Pk2_Rsvp.Models
+{
+    public class GuestResponse
+    {
+        [Required (ErrorMessage="Por Favor ingrese su nombre.")]
+        public string Name { get; set; }
+
+        [Required (ErrorMessage="Por favor ingrese su correo electronico.")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage="Favor de ingresar una nueva cuenta de correo valida")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage="Por favor ingrese su número telefónico.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage="Por favor especifique si asistira o no.")]
+        public bool? WillAttend { get; set; }
+    }
+}
