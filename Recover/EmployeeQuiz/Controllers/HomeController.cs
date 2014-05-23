31f57a8c812f5dde22ac28c39a82c6228635cd4d
Linet <lilian_01_6@hotmail.com>
2014-05-22 20:51:23 -0500

    Recover

---
+++ Recover/EmployeeQuiz/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using EmployeeQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeQuiz.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Employee emp)
+        {
+            //creo el objeto
+            //del modelo de datos
+            PayrollDm nomina = new PayrollDm(
+                @"C:\Users\Linet\documents\visual studio 2012\Projects\Recover\EmployeeQuiz\Models\Bd_Empleados.csv");
+
+            //Busco el empleado dado su ID
+            var empleado = nomina.GetEmployeeById(emp.Id);
+            if (ModelState.IsValid)
+            {
+
+                return View("WorkerView", empleado);
+            }
+            else 
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ Recover/EmployeeQuiz/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EmployeeQuiz.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Introduce tu ID:")]
+        public string Id{ get; set; }//Representa las propiedades de un Empleado 
+
+        public string Name { get; set; }//Modela a una persona
+        public string FirstLastname { get; set; }
+        public string SecondLastname { get; set; }
+        public string Position { get; set; }
+        public double Wage { get; set; }
+        public char Sex{ get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
