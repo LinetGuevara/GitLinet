25ded3187e3198b26b336185b7ec2ae0fc3fa7e6
Linet <lilian_01_6@hotmail.com>
2014-06-05 22:01:49 -0500

    MeHasVisto1

---
+++ MeHasVisto_1/MeHasVisto/Controllers/PetController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeHasVisto.Controllers
+{
+    public class PetController : Controller
+    {
+        //
+        // GET: /Pet/
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Display()
+        {
+            
+            var name = (string)RouteData.Values["id"];
+            object model = null;
+            if (model == null)
+            return RedirectToAction("NotFound");
+            return View(); // View(model);
+
+           
+        }
+        public ActionResult NotFound()
+        {
+            ViewBag.ErrorCode = "NFE00001";
+            ViewBag.Description = "La mascota no se encuentra" +
+                "en la base de datos";
+            return View();
+
+           // ViewData["ErrorCode"] = "NFE0001";
+            //ViewData["Description"] = "La masocota no se encuentra" +
+               // "en la base de datos";
+            //return View();
+        }
+
+        public FileResult DownLoadPicture()
+        {
+            //Descargar cierta imagen nos retorna en unservidor
+            var name = (string)RouteData.Values["id"];
+            var picture = @"C:\Users\Linet\Pictures/" + name + ".jpg";
+            //var picture = "/Content/Uploads/" + name + ".jpg";
+            var contentType = "image/jpg";
+            var fileName = name + ".jpg";
+            return File(picture, contentType, fileName);
+        }
+
+        public HttpStatusCodeResult UnauthorizedError()
+        {
+            return new HttpUnauthorizedResult("Error de acceso no aturizado");
+        }
+
+        public ActionResult NotFoundError()
+        {
+            return HttpNotFound("Nada por aqui....");
+        }
+
+        //Action Mathod que permite la carga 
+        //de la foto de ua mascota
+        public ActionResult PictureUpload()
+        {
+            return View();
+        }
+    }
+}
---
+++ MeHasVisto_1/MeHasVisto/Models/Bussines/PetManagement.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace MeHasVisto.Models.Bussines
+{
+    public class PetManagement
+    {
+        public static void CreateThumbNail(
+            string fileName, string filePath,
+            int thumbWi, int thumbHi,
+            bool maintainAspec)
+        {
+            //No hacer  nada si el tama;o
+            //Original es mas peque;o que
+            //el designado para la
+            //vista previa(thumbnail)
+
+            var originalFile = Path.Combine(filePath,
+                fileName);
+            var source = Image.FromFile(originalFile);
+            if (source.Width <= thumbWi && //Nos pregunta si el tamaño de la imagen es mas chica en ancho y alto no hagas nada
+                source.Height <= thumbHi)
+                return;
+
+              Bitmap thumbnail;
+        try
+    {
+        int wi = thumbWi;
+        int hi = thumbHi;
+        if(maintainAspec)
+    {
+        //Mantener el aspecto a pesar
+        //de los parametros de tamaño
+        //de la vista previa
+         wi =thumbWi;
+
+        hi = (int)(source.Height *
+            ((decimal)thumbWi / source.Width));
+    }
+    else
+    {
+       hi = thumbHi;
+       wi =(int)(source.Width *
+           ((decimal)thumbHi /source.Height));
+    }
+
+    thumbnail = new Bitmap(wi, hi);
+    using(Graphics g = Graphics.FromImage(thumbnail))
+    {
+      g.InterpolationMode =
+        InterpolationMode.HighQualityBicubic;
+       g.FillRectangle(Brushes.Transparent,
+        0, 0,
+       wi,  hi);
+
+    g.DrawImage(source, 0,0, wi, hi);
+    }
+    var thumbnailName = Path.Combine(filePath,
+       "thumbnail_" + fileName);
+    thumbnail.Save(thumbnailName);
+    }
+    catch
+     {
+     }
+        }
+
+    }
+}
